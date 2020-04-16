2f01a11d8e0af1a638782ec0a70c17b0e1423193
osirismoraes <60973011+osirismoraes@users.noreply.github.com>
2020-04-16 17:44:38 +0100

    improving the code

--- analysis.py
+++ analysis.py
@@ -3,21 +3,16 @@ import sys
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-
-# reading iris.csv
-data = pd.read_csv("https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data", header=None)
-df = pd.DataFrame(data)
-print(df)
-
-# outputs a summary of each variable to a single text file
-df.columns = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'species']
-print(df.columns)
+df = pd.read_csv("https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data", header=None) # reading iris.csv
+df.columns = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'species'] # organizing nomenclature
 df.replace({'Iris-setosa': 'setosa'}, regex=True, inplace=True)
 df.replace({'Iris-versicolor': 'versicolor'}, regex=True, inplace=True)
 df.replace({'Iris-virginica': 'virginica'}, regex=True, inplace=True)
 
-# 1 - summary
-save_exit = sys.stdout
+
+# 1 - Summary
+
+save_exit = sys.stdout # outputs a summary of each variable to a single text file
 file = open('Summary.txt', 'w')
 sys.stdout = file
 w = df.head(10)
@@ -26,27 +21,20 @@ print(df.info())
 sys.stdout = save_exit
 file.close()
 
-# 3 - division of `Species`
-a = df[df['species'].str.contains("setosa")].groupby('species').size()
-b = df[df['species'].str.contains("setosa")].groupby('species').size()
-c = df[df['species'].str.contains("setosa")].groupby('species').size()
-print(a)
-print(b)
-print(c)
 
-# saves a histogram of each variable to png files
-# 1 - histogram - the distribution of values for petal length are different for each class.
+# 2 - Histogram
+
 df.groupby('species')['petal_length'].sum().plot(kind='barh', legend='Histogram of Iris Petal Length')
 plt.xlabel('length')
 plt.ylabel('frequency')
-plt.savefig('Iris_histogram.png', transparent=True)
+plt.savefig('Iris_histogram.png', transparent=True) # saves a histogram of each variable to png files
 plt.show()
 plt.close()
 
-# outputs a scatter plot of each pair of variables
-# 1 - matrix plots shows the data and its distribution
 
-new = df[['species','petal_length', 'sepal_length']]
+# 3 - Scatter - matrix plots shows the data and its distribution (https://pandas.pydata.org/docs/reference/api/pandas.plotting.scatter_matrix.html)
+
+new = df[['species','petal_length', 'sepal_length', 'petal_width', 'sepal_width']] # outputs a scatter plot of each pair of variables
 sns.set(style='ticks', color_codes=True)
 g = sns.pairplot(new, hue='species', palette='Spectral')
 plt.savefig('Iris_scatter.png', transparent=True)
